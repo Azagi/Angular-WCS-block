82304a2e3617cbeea9c9c5e4c0b0189c8bd1f597
Filip <f07.sudol@gmail.com>
2024-05-03 10:58:26 +0200

    block angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { BlockComponentsComponent } from './block-components/block-components.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BlockComponentsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/block-components/block-components.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-block-components',
+  standalone: true,
+  imports: [],
+  templateUrl: './block-components.component.html',
+  styleUrl: './block-components.component.css'
+})
+export class BlockComponentsComponent {
+
+}
